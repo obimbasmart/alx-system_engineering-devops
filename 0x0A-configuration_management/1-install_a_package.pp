8df20740052f5987513958186dbb1855392e5731
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-11-28 21:52:09 +0100

    fix: incorrrect configuration

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# install flask from pip3
+# Install Werkzeug 2.1.1
 
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+}
+
+# install flask from pip3
 package { 'flask':
     ensure   => '2.1.0',
-    provider => 'pip3'
+    provider => 'pip3',
 }

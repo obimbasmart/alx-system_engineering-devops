9ee04b43fc2c73ef7d8def8890ee096679a07f54
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-11 22:03:59 +0100

    fix: error

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 # configure a new nginx web server and add custom HTTP header
 
-# install nginx
 exec { 'update_repo':
   command => 'sudo apt-get -y update',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 }
 
-exec { 'install_nginx':
-  command => 'sudo apt-get -y install nginx',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+# install nginx
+package { 'nginx':
+  ensure   => installed,
+  provider => 'apt'
 }
+
 
 # change permission
 file { '/var/www/html/index.nginx-debian.html':
   ensure  => present,
   content => 'Breadwinner why ?!',
   recurse => true,
-  owner   => 'ubuntu',
-  group   => 'ubuntu'
+  owner   => $facts['identity']['user'],
+  group   => $facts['identity']['user']
 }
 
 exec { 'change_file_permission':
@@ -26,56 +27,18 @@ exec { 'change_file_permission':
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 }
 
-file { '/etc/nginx/nginx.conf':
-  ensure  => present,
-  content => '
-user www-data;
-worker_processes auto;
-pid /run/nginx.pid;
-include /etc/nginx/modules-enabled/*.conf;
-
-events {
-	worker_connections 768;
-	# multi_accept on;
-}
-
-http {
-
-	##
-	# Basic Settings
-	##
-
-	sendfile on;
-	tcp_nopush on;
-	tcp_nodelay on;
-	keepalive_timeout 65;
-	types_hash_max_size 2048;
-	# server_tokens off;
-	add_header X-Served-By $hostname;
-
-
-	include /etc/nginx/mime.types;
-	default_type application/octet-stream;
-
-	ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3; # Dropping SSLv3, ref: POODLE
-	ssl_prefer_server_ciphers on;
-
-	access_log /var/log/nginx/access.log;
-	error_log /var/log/nginx/error.log;
-
-	gzip on;
-
-	include /etc/nginx/conf.d/*.conf;
-	include /etc/nginx/sites-enabled/*;
-}',
+exec { 'add_served_by_header':
+  command => "sed -i '/# server_tokens off;/a add_header X-Served-By ${hostname};' /etc/nginx/nginx.conf",
+  unless  => "grep -q 'add_header X-Served-By' /etc/nginx/nginx.conf",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 }
 
 
 exec { 'check_configuration':
-  command => 'sudo nginx -t',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+  command   => 'sudo nginx -t',
+  path      => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin',
+  logoutput => true
 }
-
 
 exec { 'restart_nginx':
   command => 'sudo service nginx restart',

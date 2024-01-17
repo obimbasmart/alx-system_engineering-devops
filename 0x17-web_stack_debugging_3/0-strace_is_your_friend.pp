759691e7e8eb34504ecc3957f1de672e0bc946eb
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-17 20:08:45 +0100

    fix: apache 500 error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,14 @@
+# errorr: Apache is returning a 500 error
+# fix: typo error: '.phpp' instead of '.php'
+
+
+exec { 'fix-typo-sed':
+  command => 'sed -i s/\.phpp/.php/g /var/www/html/wp-settings.php',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+exec { 'restart-apache2':
+  command => 'sudo service apache2 restart',
+  path    => ['usr/bin', 'usr/sbin']
+}
+

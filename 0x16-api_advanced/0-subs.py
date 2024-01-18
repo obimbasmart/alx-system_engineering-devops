6b614f961b40896d20c77e5177783b48644d97ef
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-18 16:50:35 +0100

    fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """return number of subscribers for a subreddit"""
 
     headers = {'User-Agent': 'my-app/0.0.1 by void1x2x11'}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json?raw_json=1"
+    url = f"https://www.reddit.com/r/{subreddit}/about/.json?raw_json=1"
     response = requests.get(url, headers=headers)
 
     subscribers = int(response.json()['data']['subscribers']) \

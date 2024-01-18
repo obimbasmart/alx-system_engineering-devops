3b0cb7db46c9f2e9bc28c0d28b816221fc055da7
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-18 16:23:03 +0100

    add: new task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns the number of subscribers
+for a given subreddit"""
+
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    """return number of subscribers for a subreddit"""
+
+    headers = {'user-agent': 'my-app/0.0.1'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json?raw_json=1"
+    response = requests.get(url, headers=headers)
+
+    subscribers = int(response.json()['data']['subscribers']) \
+        if response.status_code == 200 else 0
+
+    return subscribers

c8dfe33435de4a6c05e20b4d2ddc20afa7afe3c3
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-30 14:33:33 +0100

    update: puppet manifest

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,7 @@
 ## fix `Too many open file` error on nginx
 
-
-exec { 'change-ULIMIT':
-  command => 'sed -i "s/ULIMIT=\"-n 321\"/ULIMIT=\"-n 9999\"/" /etc/default/nginx',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+exec { 'Limit':
+  command => '/usr/bin/env sed -i s/15/2000/ /etc/default/nginx',
 }
 
-# restart nginx
-exec { 'nginx-restart':
-  command => 'service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-}
-
+exec { '/usr/bin/env service nginx restart': }
